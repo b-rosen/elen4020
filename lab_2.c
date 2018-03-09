d53c7e14761254d7ae59afdee0ff1fe12cb4ed6a
Benjy Rosen <858324@students.wits.ac.za>
2018-03-09 09:53:22 +0200

    Fixed some stuff

--- lab_2.c
+++ lab_2.c
@@ -10,8 +10,8 @@ void PrintMatrix(int *arr, int xSize, int ySize)
   int length = DimensionsToLength(xSize, ySize);
   for (size_t i = 0; i < length; i++)
   {
-    printf("%d ", arr[i]);
-    if (i % xSize == 0)
+    printf("%d\t", arr[i]);
+    if ((i+1) % xSize == 0)
       printf("\n");
   }
 }
@@ -22,14 +22,14 @@ void SetNumber(int *arr, int xSize, int ySize)
 
   for (int i = 0; i < length; i++)
   {
-    arr[i] = i;
+    arr[i] = i+1;
   }
 }
 
 int main(int argc, char const *argv[])
 {
   printf("Using A[10][10] Array:\n\n");
-  int dimension = 10;
+  int dimension = 5;
   int *testArr = malloc(DimensionsToLength(dimension, dimension)*sizeof(int));
   SetNumber(testArr, dimension, dimension);
   PrintMatrix(testArr, dimension, dimension);
